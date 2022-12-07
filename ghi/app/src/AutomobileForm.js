b233d4e3f827d199c005172d7b14d755c1759e49
Khaled <khaled.azimi@gmail.com>
2022-12-07 11:16:51 -0800

    fixed create auto

--- ghi/app/src/AutomobileForm.js
+++ ghi/app/src/AutomobileForm.js
@@ -1,131 +1,110 @@
 import React, {useEffect, useState} from 'react';
 
-class AutomobileForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      color: '',
-      year: '',
-      vin: '',
-      model: '',
-      models: [],
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChangeColor = this.handleChangeColor.bind(this);
-    this.handleChangeYear = this.handleChangeYear.bind(this);
-    this.handleChangeVin = this.handleChangeVin.bind(this);
-    this.handleChangeModel = this.handleChangeModel.bind(this);
-  }
+const AutomobileForm = () => {
 
 
-  async componentDidMount() {
-    const url = `http://localhost:8100/api/models/`;
+    const [color, setColor] = useState('');
+    const [year, setYear] = useState('');
+    const [vin, setVin] = useState('');
+    const [model_id, setModel] = useState('');
+    const [models, setModels] = useState([]);
 
-    const response = await fetch(url);
 
-    if (response.ok) {
-      const data = await response.json();
-      this.setState({ models: data.models });
+    useEffect(() => {
+        const modelsUrl = 'http://localhost:8100/api/models/';
+        fetch(modelsUrl)
+            .then(response => response.json())
+            .then(data => setModels(data.models))
+            .catch(e => console.error('error: ', e))
+    }, [])
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newAuto = {
+            'color': color,
+            'year': year,
+            'vin': vin,
+            'model_id': model_id,
+        }
+
+        console.log(newAuto)
+
+        const autoUrl = "http://localhost:8100/api/automobiles/";
+        const fetchConfig = {
+            method: "post",
+            body: JSON.stringify(newAuto),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        fetch(autoUrl, fetchConfig)
+            .then(response => response.json())
+            .then(() => {
+                setColor('');
+                setYear('');
+                setVin('');
+                setModel('');
+            })
+            .catch(e => console.log('error: ', e));
     }
-  }
-
-  async handleSubmit(event) {
-    event.preventDefault();
-    const data = {...this.state};
 
 
-    const autoUrl = 'http://localhost:8100/api/automobiles/';
-    const fetchConfig = {
-      method: "post",
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-    const response = await fetch(autoUrl, fetchConfig);
-    if (response.ok) {
-      const newAuto = await response.json();
-      console.log(newAuto);
-
-      const cleared = {
-        color: '',
-        year: '',
-        vin: '',
-        model: '',
-      };
-
-      this.setState(cleared)
-
+    const handleColorChange = (event) => {
+        const value = event.target.value;
+        setColor(value);
     }
-  }
 
-  handleChangeColor(event) {
-    const value = event.target.value;
-    this.setState({ color: value });
-  }
+    const handleYearChange = (event) => {
+        const value = event.target.value;
+        setYear(value);
+    }
 
-  handleChangeYear(event) {
-    const value = event.target.value;
-    this.setState({ year: value });
-  }
+    const handleVinChange = (event) => {
+        const value = event.target.value;
+        setVin(value);
+    }
 
-  handleChangeVin(event) {
-    const value = event.target.value;
-    this.setState({ vin: value });
-  }
+    const handleModelChange = (event) => {
+        const value = event.target.value;
+        setModel(value);
+    }
 
-  handleChangeModel(event) {
-    const value = event.target.value;
-    this.setState({ model: value });
-  }
-
-
-
-  render() {
     return (
-        <>
+        <div className="row">
+            <div className="offset-3 col-6">
+                <div className="shadow p-4 mt-4">
+                    <h1>Create a Automobile</h1>
+                    <form onSubmit={handleSubmit} id="create-bin-form">
+                        <div className="form-floating mb-3">
+                            <input value={color} onChange={handleColorChange} required type="text" name="color" id="color" className="form-control" />
+                            <label>Color</label>
+                        </div>
+                        <div className="form-floating mb-3">
+                            <input value={year} onChange={handleYearChange} required type="text" name="year" id="year" className="form-control" />
+                            <label>Year</label>
+                        </div>
+                        <div className="form-floating mb-3">
+                            <input value={vin} onChange={handleVinChange} required type="text" name="vin" id="vin" className="form-control" />
+                            <label>Vin</label>
+                        </div>
+                        <div className="mb-3">
+                            <select onChange={handleModelChange} value={model_id} required id="model" name="model" className="form-select">
+                                <option value="">Choose a model</option>
+                                {models.map(model => {
+                                    return (
+                                        <option key={model.id} value={model.id}>{model.name}</option>
+                                    );
+                                })}
+                            </select>
+                        </div>
 
-
-      <div className="row">
-        <div className="offset-3 col-6">
-          <div className="shadow p-4 mt-4">
-            <h1>Create a new Automobile</h1>
-            <form onSubmit={this.handleSubmit} id="create-auto-form">
-              <div className="form-floating mb-3">
-                <input onChange={this.handleChangeColor} value={this.state.color} placeholder="color" required type="text" name="color" id="color" className="form-control" />
-                <label htmlFor="fabric">Color</label>
-              </div>
-              <div className="form-floating mb-3">
-                <input onChange={this.handleChangeYear} value={this.state.year} placeholder="Year" required type="number" name="year" id="year" className="form-control" />
-                <label htmlFor="style">Year</label>
-              </div>
-              <div className="form-floating mb-3">
-                <input onChange={this.handleChangeVin} value={this.state.vin} placeholder="Vin" required type="text" name="vin" id="vin" className="form-control" />
-                <label htmlFor="color">Vin</label>
-              </div>
-              <div className="mb-3">
-                <select onChange={this.handleChangeModel} value={this.state.model} required name="model" id="model" className="form-select">
-                  <option value="">Choose a Model</option>
-                  {this.state.models.map(model => {
-                    return (
-                      <option key={model.id} value={model.id}>{model.name}</option>
-                    )
-                  })}
-                </select>
-              </div>
-              <button className="btn btn-primary">Create</button>
-            </form>
-            <br></br>
-            <a href="/automobiles">
-                <button className="btn btn-outline-danger">Back to Automobile list</button>
-            </a>
-          </div>
+                        <button className="btn btn-primary">Create</button>
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
-      </>
     );
-  }
 }
 
 export default AutomobileForm;
