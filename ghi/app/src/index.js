e0d0da61a36dcb52022698b10078872eca037996
Gabe <Gabrielaranda202@gmail.com>
2022-12-07 11:57:00 -0800

    Fixed VehicleModelForm usingreact hooks, cleared index.js, as we are now fetching our data on the corresponding list.js

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import VehicleModelForm from './VehicleModelForm';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,21 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-async function loaddata() {
-  const modelResponse = await fetch("http://localhost:8100/api/models/")
-  if  (modelResponse.ok) {
-    const modelData = await modelResponse.json();
-    console.log(modelData)
-    root.render(
-      <React.StrictMode>
-        <App models={modelData.models}/>
-      </React.StrictMode>
-    )
-  } else {
-    console.error(modelResponse)
-  }
-}
-
-loaddata();
