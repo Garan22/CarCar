16c3014bc483b7b2cad565354804edcd937d8bc2
Khaled <khaled.azimi@gmail.com>
2022-12-07 18:44:23 -0800

    commit work on encoders

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -9,6 +9,8 @@ class AutomobileVO(models.Model):
     year=models.PositiveSmallIntegerField()
     vin = models.CharField(max_length=17, unique=True)
 
+    vehicleSold = models.BooleanField(default=False)
+
     def __str__(self):
         return self.vin
 
