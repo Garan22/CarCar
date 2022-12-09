55fa66dde81eceb8f7c8eb7ef163835383b31139
Khaled <khaled.azimi@gmail.com>
2022-12-09 14:31:22 -0800

    committing

--- ghi/app/src/SalespersonForm.js
+++ ghi/app/src/SalespersonForm.js
@@ -50,7 +50,7 @@ const SalespersonForm = () => {
         <div className="row">
             <div className="offset-3 col-6">
                 <div className="shadow p-4 mt-4">
-                    <h1>Create a Customer</h1>
+                    <h1>Create a New Employee</h1>
                     <form onSubmit={handleSubmit} id="create-bin-form">
                         <div className="form-floating mb-3">
                             <input value={name} onChange={handleNameChange} required type="text" name="name" id="name" className="form-control" />
